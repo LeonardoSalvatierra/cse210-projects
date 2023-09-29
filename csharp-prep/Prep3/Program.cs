6a2ee87a3b7b0d8c3dfa03b5c1bded96f648f1fc
Leonardo Salvatierra <salvatierra.leonardo.sud@gmail.com>
2023-09-28 21:19:24 -0300

    completed prep3

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -4,6 +4,32 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+        Random randomgenerator = new Random();
+        int number = randomgenerator.Next(1,100);
+        string response = "no";
+        int guesses = 1;
+        do 
+        {
+            Console.Write("What is your guess?: ");
+            string guess_input = Console.ReadLine();
+            int guess_number = int.Parse(guess_input);
+
+            if (guess_number == number)
+            {
+                Console.WriteLine("You guessed it!");
+                response = "yes";
+                Console.WriteLine($"you did {guesses} guesses.");
+            }
+            else if (guess_number > number)
+            {
+                Console.WriteLine("lower!");
+            }
+            else
+            {
+                Console.WriteLine("higher!");
+            }
+            guesses = guesses + 1;
+        } while (response == "no");
+        
     }
 }
